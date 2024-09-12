702cbc589c430b4c678d022a3b3a1d97368119d1
Jane Roffer <47247206+Pisey-Nguon@users.noreply.github.com>
2024-09-12 12:03:53 +0700

    Prepare for jitpack

--- page-indicator/build.gradle.kts
+++ page-indicator/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    `maven-publish`
 }
 
 android {
@@ -40,4 +41,31 @@ dependencies {
 //    testImplementation(libs.junit)
 //    androidTestImplementation(libs.androidx.junit)
 //    androidTestImplementation(libs.androidx.espresso.core)
+}
+afterEvaluate {
+    publishing {
+        publications {
+
+            // Creates a Maven publication called "release".
+            create<MavenPublication>("release") {
+
+                // Applies the component for the release build variant.
+                // NOTE : Delete this line code if you publish Native Java / Kotlin Library
+                from(components["release"])
+
+                // Library Package Name (Example : "com.frogobox.androidfirstlib")
+                // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
+                groupId =  "com.digitaltalent"
+
+                // Library Name / Module Name (Example : "androidfirstlib")
+                // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
+                artifactId = "page-indicator"
+
+                // Version Library Name (Example : "1.0.0")
+                version = "1.0.0"
+
+            }
+
+        }
+    }
 }
